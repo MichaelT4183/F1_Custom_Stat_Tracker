9659756006d1c4c7f7770d17ce324977f60c441b
MichaelT4183 <mtabbinor123@gmail.com>
2025-11-18 14:48:38 +0000

    Added private variables and constructors

--- ChampionshipTable.java
+++ ChampionshipTable.java
@@ -1,5 +1,16 @@
+import java.util.HashMap;
+
 public class ChampionshipTable
 {
+    private HashMap<String, Integer> championshipPoints;
+    private ArrayList<String> drivers;
+
+    // Create the java constructor
+    public ChampionshipTable(ArrayList<String> drivers)
+    {
+        this.drivers = drivers;
+    }
+
     public void setChampionshipTable(ArrayList<String> drivers)
     {
         for(int i = 0; i < drivers.size(); i++)
