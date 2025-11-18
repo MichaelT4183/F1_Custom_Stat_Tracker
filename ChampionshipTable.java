665b688ebed572a922cbbe56a95870a8d6726b73
MichaelT4183 <mtabbinor123@gmail.com>
2025-11-18 15:34:51 +0000

    Create a module to get the points of the driver based off finishing place

--- ChampionshipTable.java
+++ ChampionshipTable.java
@@ -19,12 +19,32 @@ public class ChampionshipTable
         }
     }
 
-    public void appendChampionshipTable(ArrayList<String> raceFinish)
+    public void appendChampionshipTable(String[] raceFinish)
     {
         for(int i = 0; i < raceFinish.size(); i++)
         {
+            // Get the driver's name and the points for their finish position
             String driverName = raceFinish.get(i);
+            int driverPoints = getFinishPoints(i);
+
             championshipPoints.replace();
+        }
+    }
+
+    // Method to get the points for whoever finished
+    private int getFinishPoints(int i)
+    {
+        // Check to see if the driver finished within the top 10
+        if(i < 10)
+        {
+            int[] points = {25, 18, 15, 12, 10, 8, 6, 4, 2, 1};
+            
+            return points.get(i);
+        }
+        // If driver finished out of the top 10
+        else 
+        {
+            return 0;
         }
     }
 }
