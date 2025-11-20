2795f559ccc2d93c824007eb5376a62e92ecd1cb
MichaelT4183 <mtabbinor123@gmail.com>
2025-11-20 18:25:56 +0000

    Successfully used the constructor

--- Test_File_DriverAndRaceDetails.java
+++ Test_File_DriverAndRaceDetails.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class Test_File_DriverAndRaceDetails
 {
+    // Main method used to test the file
     public static void main(String[] args)
     {
         // Get the user's name
         Scanner scanner = new Scanner(System.in);
         System.out.println("Test");
         String name = scanner.nextLine();
+        // Test the constructor and that it works
         DriverAndRaceDetails details = new DriverAndRaceDetails(name);
+        System.out.println(details);
 
 
-        
         scanner.close();
     }
 
