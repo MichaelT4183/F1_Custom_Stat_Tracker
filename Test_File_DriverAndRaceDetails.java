d0672e918b38a92ffd4d49e9a4f958ab37ad3e16
MichaelT4183 <mtabbinor123@gmail.com>
2025-11-20 18:13:06 +0000

    Added a scanner for user input and created constructor test

--- Test_File_DriverAndRaceDetails.java
+++ Test_File_DriverAndRaceDetails.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Test_File_DriverAndRaceDetails
 {
     public static void main(String[] args)
     {
+        // Get the user's name
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Test");
+        String name = scanner.nextLine();
+        DriverAndRaceDetails details = new DriverAndRaceDetails(name);
+
+
         
+        scanner.close();
     }
+
 }
