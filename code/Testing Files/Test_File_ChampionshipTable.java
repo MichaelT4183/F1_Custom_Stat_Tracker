095d8435d5981662b7fe3d2c4c723528f362286c
MichaelT4183 <mtabbinor123@gmail.com>
2025-11-21 22:49:39 +0000

    Created the test file and created tests to ensure that the file works

--- code/Testing Files/Test_File_ChampionshipTable.java
+++ code/Testing Files/Test_File_ChampionshipTable.java
@@ -3,9 +3,26 @@ import seasonMethods.ChampionshipTable; // Import the package for testing
 
 public class Test_File_ChampionshipTable
 {
+    // Main method used for testing
     public static void main(String[] args) 
     {
         ArrayList<String> drivers = new ArrayList<>(); // ArrayList for test data (Driver names)
+        // Test the constructor
         ChampionshipTable table = new ChampionshipTable(drivers);
+
+        // Test to try and print the championship table before populating it (Should return an error)
+        System.out.println(table);
+
+        // Populate the driver's ArrayList for test data
+        drivers.add("Lando");
+        drivers.add("Charles");
+        drivers.add("Max");
+
+        // Create the race finish and attempt to append the championship table
+        String raceFinish[] = {"Lando", "Max", "Charles"}; // Array for test data (Race finish)
+        table.appendChampionshipTable(raceFinish);
+
+        // Check to see if the championship table has been altered
+        System.out.println(table);
     }
 }
