4bfe552e4414ede06877ffcfd4ae0700689b2307
MichaelT4183 <mtabbinor123@gmail.com>
2025-11-24 13:40:08 +0000

    Added class and constructor
    Added the class name and the java constructor. Initialized the required variables in the constructor and the class

--- code/seasonMethods/RaceSimulator.java
+++ code/seasonMethods/RaceSimulator.java
@@ -1,1 +1,16 @@
+import java.util.ArrayList; // Used for the driver and race lists
 
+public class RaceSimulator
+{
+    private ArrayList<String> drivers; // Get the drivers competing in the season
+    private ArrayList<String> races; // Get the number of races and the race names that are in the season
+    private String raceFinish[] = new String [drivers.size()]; // Create an array for where the drivers finish the race (length based on number of drivers)
+
+    // Create the java constructor
+    public RaceSimulator(ArrayList<String> drivers, ArrayList<String> races)
+    {
+        // Initialize the variables
+        this.drivers = drivers;
+        this.races = races;
+    }
+}
