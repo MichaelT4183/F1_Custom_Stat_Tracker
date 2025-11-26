ba22f715a37b5f3ae6fc4cbab07c11c750b33d73
MichaelT4183 <mtabbinor123@gmail.com>
2025-11-26 10:55:14 +0000

    Added test data and variables. Created the main class to run the testing file

--- code/Testing Files/Test_File_RaceSimulator.java
+++ code/Testing Files/Test_File_RaceSimulator.java
@@ -1,1 +1,27 @@
+import packages.seasonMethods.RaceSimulator;
+import java.util.ArrayList;
 
+public class Test_File_RaceSimulator
+{
+    // Main method to run the test file
+    public static void main(String[] args)
+    {
+        // Create test data variables
+        ArrayList<String> drivers = new ArrayList<>(); // ArrayList for the drivers
+        ArrayList<String> races = new ArrayList<>(); // ArrayList for the races
+
+        // Add test data to the drivers ArrayList
+        drivers.add("Lando");
+        drivers.add("Max");
+        drivers.add("Oscar");
+        drivers.add("Kimi");
+        drivers.add("Carlos");
+
+        // Add test data to the races ArrayList
+        races.add("Silverstone");
+        races.add("Bahrain");
+        races.add("Jeddah");
+        races.add("Spa");
+        races.add("Abu Dhabi");
+    }
+}
