9d4860f5109f37f5de4a04442484275e9c917cb0
MichaelT4183 <mtabbinor123@gmail.com>
2025-12-01 09:56:50 +0000

    Added extra comments and outputs to MainClass.java

--- code/MainClass.java
+++ code/MainClass.java
@@ -4,6 +4,11 @@ import packages.seasonMethods.RaceSimulator;
 import java.util.Scanner; // Used for user input
 import java.util.ArrayList; // Used to store the drivers and the races
 
+/* This class is designed to be the main program that is ran for the user to
+use the custom F1 season simulator. This file uses code from the test files
+as they show that the code works and does not produce errors. When a user
+is using the program, this is the file that uses all the package methods
+and classes */
 public class MainClass
 {
     public static void main(String[] args)
@@ -22,7 +27,9 @@ public class MainClass
         ArrayList<String> races = driversAndRaces.getRaceNames();
         System.out.println(driversAndRaces);
 
-        // Create a menu for the season simulator to give users a choice on what to do
+        /* Menu for the season simulator to give users a choice on what to do.
+        This will end either when the user closes the program or when the program runs
+        out of races */
         RaceSimulator simRace = new RaceSimulator(drivers, races);
         int userInput;
         int i = 0;
@@ -30,27 +37,30 @@ public class MainClass
         {
             System.out.println("\nPlease choose from one of the following options\n");
             System.out.println("1) See drivers and races");
-            System.out.println("2) Simulate the season");
+            System.out.println("2) Simulate the next race in the season");
             System.out.println("3) View the championship table");
             System.out.println("4) End the program");
             userInput = scanner.nextInt();
 
             if(userInput == 1)
             {
+                // Display the drivers and races
                 System.out.println(driversAndRaces);
             }
             else if(userInput == 2)
             {
-                // Simulate a races
+                // Simulate a race
                 simRace.simulateRace(i);
                 i++;
             }
             else if(userInput == 3)
             {
+                // Display the championship table
                 simRace.displayChampionshipTable();
             }
             else if(userInput == 4)
             {
+                // Ends the program
                 System.out.println("\nThank you for using this simulator");
                 System.out.println("A file has been created to store all of the season information");
                 System.exit(0);
@@ -62,6 +72,7 @@ public class MainClass
             }
         }
         // Goodbye message when the season has been fully simulated
+        System.out.println("\nAll of the races have been simulated\n");
         System.out.println("\nThank you for using this simulator");
         System.out.println("A file has been created to store all of the season information");
         System.exit(0);
